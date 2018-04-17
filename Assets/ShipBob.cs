92a0877a50e3fa43f31f7c0a8f7129d06adc8070
Linus Waddell <linuswaddell@gmail.com>
2018-04-17 00:44:14 -0700

    Add ship bob script

---
+++ Assets/ShipBob.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBob : MonoBehaviour {
+
+	float normalPosY;
+	public float bobStrength = 1;
+	public float bobSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		normalPosY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x,
+			normalPosY + ((float)Mathf.Sin((2 * Mathf.PI * bobSpeed) * Time.time) * bobStrength),
+			transform.position.z);
+	}
+}

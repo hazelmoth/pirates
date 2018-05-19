f9d402a4fdbe0f5541b16667dbba4d722ee00d18
Linus Waddell <linuswaddell@gmail.com>
2018-05-19 01:10:28 -0700

    Add ship rocking

--- Assets/ShipBob.cs
+++ Assets/ShipBob.cs
@@ -18,5 +18,10 @@ public class ShipBob : MonoBehaviour {
 		transform.position = new Vector3(transform.position.x,
 			normalPosY + ((float)Mathf.Sin((2 * Mathf.PI * bobSpeed) * Time.time) * bobStrength),
 			transform.position.z);
+		transform.rotation = Quaternion.Euler (new Vector3 (0.128f * curve(Mathf.Sin ((2 * Mathf.PI * bobSpeed) * Time.time + Mathf.PI / 2)) * bobStrength * 5 + 0.31f, (float)this.transform.rotation.eulerAngles.y, 0f));
+	}
+
+	float curve (float x) {
+		return (float)(Mathf.Pow (1f - x, 3f) * -2.3f) + (3f * Mathf.Pow (1f - x, 2f) * x * 1.3f) + (3f * (1f - x) * Mathf.Pow (x, 2f) * 4.86f) + (Mathf.Pow (x, 3f) * 5.3f);
 	}
 }

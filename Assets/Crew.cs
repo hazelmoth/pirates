6ace28223df60c54b8849a5f83c8a9b7aa48a609
Linus Waddell <linuswaddell@gmail.com>
2018-05-28 17:19:22 -0700

    Add Crew class for handling crews

---
+++ Assets/Crew.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crew : Object {
+
+	private string shipName;
+
+	private List<Player> members;
+	private Player captain;
+	private Ship ship;
+
+	public Crew(Player captain, Ship ship) {
+		this.captain = captain;
+		this.ship = ship;
+	}
+
+	public void AddMember(Player player) {
+		members.Add (player);
+	}
+}
